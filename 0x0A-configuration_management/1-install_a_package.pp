f87472026329440636b62b24ffaa48429c826469
cynthiaimari <cynthiaimari@gmail.com>
2024-04-21 13:36:34 +0300

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+#This puppet mainfest installs flask from pip3
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
 }
