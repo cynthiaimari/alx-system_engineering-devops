7d94d28b790997c13f9ecc07d9ac59a235710daa
cynthiaimari <cynthiaimari@gmail.com>
2024-04-21 13:46:13 +0300

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-# Puppet script to create ssh config file
+#This puppet mainfest configures using private key ~/.ssh/school
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
@@ -9,3 +9,4 @@ file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
+}
